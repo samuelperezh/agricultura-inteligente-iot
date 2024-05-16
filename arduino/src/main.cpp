89728e69454a8b78c5f404cceea476af73933468
Samuel Perez Hurtado <samuelperezh22@gmail.com>
2024-05-16 14:06:04 -0500

    Actualización sensores

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -12,20 +12,20 @@
 static void smartDelay(unsigned long ms);
 void send_data(const char* id, float temperatura, float humedad, float latitud, float longitud);
 void leerGPS(void);
-float leerH(void);
-float leerT(void);
-float prunningH(int n);
-float prunningT(int n);
-float readMoisture(void);
-long alsValue;
-long psValue;
+float leerHumedadAmbiental(int n);
+float leerTemperatura(int n);
+float leerLuz(int n);
+float leerProximidad(int n);
+float leerHumedadPlanta(int n);
+const int analogPin = 33;
+const int ledPin = 25;
 static const uint32_t GPSBaud = 9600;
 const char* server = "18.212.13.195";
-const char* ssid = "UPBWiFi";
+const char* ssid = "PerezHurtado";
 const char* id = "point06";
 int estado = 0;
 const int prunning = 10;
-float temperatura, humedad, latitud, longitud;
+float temperatura, humedad, luz, proximidad, humedad_planta, latitud, longitud;
 int moisturePin = 33;
 float moistureLevel;
 
@@ -44,9 +44,10 @@ void setup() {
   sensorHT.begin(0x40);
   aps.begin();
 	aps.startAmbientLightAndProximitySensor ();
-  pinMode(moisturePin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, "");
+  WiFi.begin(ssid, "Mencha707150*");
   Serial.println("Conectando a la red WiFi");
   while (WiFi.status() != WL_CONNECTED) {
     Serial.println("Intentando conectar a la red WiFi...");
@@ -59,18 +60,12 @@ void setup() {
 void loop() {
   switch(estado) {
     case 1:
-      Serial.println("1. Leyendo datos del sensor de Humedad y Temperatura");
-      temperatura = leerT();
-      humedad = leerH();
-      alsValue = aps.getAmbientLight();
-	    psValue = aps.getProximity();
-      Serial.print("Luz: ");
-      Serial.print(alsValue);
-      Serial.print(", Proximidad: ");
-      Serial.println(psValue);
-      moistureLevel = readMoisture();
-      Serial.print("Soil Moisture: ");
-      Serial.print(moistureLevel);
+      Serial.println("1. Leyendo datos de los sensores de Humedad, Temperatura, Luz y Proximidad");
+      temperatura = leerTemperatura(10);
+      humedad = leerHumedadAmbiental(10);
+      luz = leerLuz(10);
+      proximidad = leerProximidad(10);
+      humedad_planta = leerHumedadPlanta(10);
       estado = 2;
       break;
     case 2:
@@ -85,7 +80,7 @@ void loop() {
       break;
     case 4:
       Serial.println("4. Durmiendo");
-      delay(10000);
+      delay(1000);
       estado = 1;
       break;
     default:
@@ -151,11 +146,15 @@ void leerGPS() {
   smartDelay(100);
 }
 
-// Función para leer datos de la humedad
-float leerH() {
-  Serial.println("Se va a leer la humedad");
-  Serial.print("La humedad promedio es de: ");
-  float humedad = prunningH(10);
+// Función para leer datos de la humedad ambiental
+float leerHumedadAmbiental(int n) {
+  Serial.print("La humedad ambiental promedio es de: ");
+  float hum = 0;
+  for (int i = 0; i < n; i++) {
+    hum += sensorHT.readHumidity();
+    delay(8);
+  }
+  float humedad = hum / n;
   Serial.print(humedad);
   Serial.println("%");
   delay(30);
@@ -163,37 +162,68 @@ float leerH() {
 }
 
 // Función para leer datos de la temperatura
-float leerT() {
-  Serial.println("Se va a leer la temperatura");
-  Serial.print("La temperatura promedio es de: ");
-  float temperatura = prunningT(10);
+float leerTemperatura(int n) {
+  Serial.print("La temperatura ambiental promedio es de: ");
+  float temp = 0;
+  for (int i = 0; i < n; i++) {
+    temp += sensorHT.readTemperature();
+    delay(8);
+  }
+  float temperatura = temp/n;
   Serial.print(temperatura);
   Serial.println(" °C");
   delay(30);
   return temperatura;
 }
 
-// Función prunning humedad
-float prunningH(int n) {
+// Función para leer datos de la luz
+float leerLuz(int n) {
+  Serial.print("La luz promedio es de: ");
+  float luz = 0;
+  for (int i = 0; i < n; i++) {
+    luz += aps.getAmbientLight();
+    delay(13);
+  }
+  float luzAmbiente = luz / n;
+  Serial.print(luzAmbiente);
+  Serial.println(" lux");
+  delay(30);
+  return luzAmbiente;
+}
+
+// Función para leer datos de la proximidad
+float leerProximidad(int n) {
+  Serial.print("La proximidad promedio es de: ");
+  float prox = 0;
+  for (int i = 0; i < n; i++) {
+    prox += aps.getProximity();
+    delay(13);
+  }
+  float proximidad = prox / n;
+  Serial.print(proximidad);
+  Serial.println(" mm");
+  delay(30);
+  return proximidad;
+}
+
+// Función para leer datos de la humedad de la planta
+float leerHumedadPlanta(int n) {
+  Serial.print("La humedad de la planta promedio es de: ");
   float hum = 0;
   for (int i = 0; i < n; i++) {
-    hum += sensorHT.readHumidity();
+    hum += analogRead(moisturePin);
     delay(8);
   }
-  return hum / n;
-}
-
-// Función prunning temperatura
-float prunningT(int n) {
-  float temp = 0;
-  for (int i = 0; i < n; i++) {
-    temp += sensorHT.readTemperature();
-    delay(8);
+  float humedadPlanta = hum / n;
+  float porcentaje = map(humedadPlanta, 875, 800, 0, 100);
+  porcentaje = constrain(porcentaje, 0, 100);
+  Serial.print(porcentaje);
+  Serial.println("%");
+  if ((porcentaje >= 0 && porcentaje < 50) || (porcentaje > 70 && porcentaje <= 100)) {
+    digitalWrite(ledPin, LOW); // Turn on LED if within the specified ranges
+  } else {
+    digitalWrite(ledPin, HIGH); // Turn off LED if outside the specified ranges
   }
-  return temp / n;
-}
-
-float readMoisture() {
-  int sensorVal = analogRead(moisturePin);
-  return 100.00 - ((sensorVal / 4095.00) * 100.00);
+  delay(30);
+  return humedadPlanta;
 }
